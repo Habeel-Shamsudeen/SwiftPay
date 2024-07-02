74677734f375a30f406e8233138b09eb54c331af
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2024-07-02 21:12:07 +0530

    initial commit

---
+++ apps/client-app/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-4xl text-sky-800">
+      Client App
+    </div>
+  );
+}
---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="font-bold text-4xl text-red-600">
+      Merchant app
+    </div>
+  );
+}
