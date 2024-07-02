a17b8b4cf3915ec72d48c3707b039de99d084ef1
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2024-07-02 21:58:51 +0530

    added prisma

--- apps/client-app/app/page.tsx
+++ apps/client-app/app/page.tsx
@@ -1,3 +1,6 @@
+import { PrismaClient } from "@repo/db/client";
+const client = new PrismaClient();
+
 export default function Home() {
   return (
     <div className="text-4xl text-sky-800">
--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -1,3 +1,7 @@
+import { PrismaClient } from "@repo/db/client";
+const client = new PrismaClient();
+
+
 export default function Home() {
   return (
     <div className="font-bold text-4xl text-red-600">
